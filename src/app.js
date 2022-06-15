17224796c5da8a7eac11190fd897fe8e37199808
bombies <xjamesdzn@gmail.com>
2022-06-15 09:20:30 -0500

    Webhook test #94

--- src/app.js
+++ src/app.js
@@ -58,8 +58,6 @@ app.post('/premiumhook', async (req, res) => {
     try {
         const verified = computeHash(req);
         if (verified) {
-            fs.writeFileSync(`./${new Date().getTime()}.json`, JSON.stringify(req.body, null, 4));
-
             const discordID = req.body['included'][1]['attributes']['social_connections']['discord'];
             const entitledTiers = req.body['data']['relationships']['currently_entitled_tiers'];
             await handlePremiumEvents(req, res, discordID, entitledTiers);

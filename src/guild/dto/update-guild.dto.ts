cf16e5313087367b4e9392dc467d314aef4f6be6
bombies <xjamesdzn@gmail.com>
2023-02-03 19:45:27 -0500

    Implement GuildService

--- src/guild/dto/update-guild.dto.ts
+++ src/guild/dto/update-guild.dto.ts
@@ -1,6 +1,16 @@
-import {DedicatedChannel} from "../guild.schema";
+import {DedicatedChannel, GuildBannedUser, GuildPermissions, GuildToggles, RestrictedChannels} from "../guild.schema";
 
 export type UpdateGuildDto = {
-    dedicated_channel: DedicatedChannel,
-
+    dedicated_channel?: DedicatedChannel,
+    restricted_channels?: RestrictedChannels,
+    prefix?: string,
+    permissions?: GuildPermissions,
+    toggles?: GuildToggles,
+    eight_ball?: string[],
+    theme?: string,
+    banned_users?: GuildBannedUser[],
+    autoplay?: boolean,
+    log_channel?: number,
+    twenty_four_seven_mode?: boolean,
+    locale?: string
 }

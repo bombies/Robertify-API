48af28ccb276b84b89c183675cca5caee3b34054
bombies <xjamesdzn@gmail.com>
2023-02-04 08:49:29 -0500

    Convert UpdateGuildDto to a class

--- src/guild/dto/update-guild.dto.ts
+++ src/guild/dto/update-guild.dto.ts
@@ -1,16 +1,16 @@
 import {DedicatedChannel, GuildBannedUser, GuildPermissions, GuildToggles, RestrictedChannels} from "../guild.schema";
 
-export type UpdateGuildDto = {
-    dedicated_channel?: DedicatedChannel,
-    restricted_channels?: RestrictedChannels,
-    prefix?: string,
-    permissions?: GuildPermissions,
-    toggles?: GuildToggles,
-    eight_ball?: string[],
-    theme?: string,
-    banned_users?: GuildBannedUser[],
-    autoplay?: boolean,
-    log_channel?: number,
-    twenty_four_seven_mode?: boolean,
-    locale?: string
+export class UpdateGuildDto {
+    dedicated_channel?: DedicatedChannel;
+    restricted_channels?: RestrictedChannels;
+    prefix?: string;
+    permissions?: GuildPermissions;
+    toggles?: GuildToggles;
+    eight_ball?: string[];
+    theme?: string;
+    banned_users?: GuildBannedUser[];
+    autoplay?: boolean;
+    log_channel?: number;
+    twenty_four_seven_mode?: boolean;
+    locale?: string;
 }

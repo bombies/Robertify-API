7af0c71ac9cbc1ffe544eef76cf50ca76048b2b6
bombies <xjamesdzn@gmail.com>
2023-02-05 08:53:51 -0500

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(Number(process.env.PORT));
 }

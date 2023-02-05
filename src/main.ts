33d893547367cf8a3c4428366e5d7c2f24382da9
bombies <xjamesdzn@gmail.com>
2023-02-05 08:53:01 -0500

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(Number(process.env.PORT));
 }

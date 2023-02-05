9783e7a7fafafb4ea1dfa48763a8e7706ff369e4
bombies <xjamesdzn@gmail.com>
2023-02-05 09:17:57 -0500

    Complete GuildController

--- src/routes/guild/guild.controller.ts
+++ src/routes/guild/guild.controller.ts
@@ -1,5 +1,6 @@
-import {Controller, Get, HttpException, HttpStatus, Param} from '@nestjs/common';
+import {Body, Controller, Get, HttpException, HttpStatus, Param} from '@nestjs/common';
 import {GuildService} from "./guild.service";
+import {UpdateGuildDto} from "./dto/update-guild.dto";
 
 @Controller('guild')
 export class GuildController {
@@ -8,6 +9,11 @@ export class GuildController {
     @Get(':id')
     async getGuild(@Param('id') id: string) {
         return await this.guildService.findOne(id);
+    }
+
+    @Get(':id')
+    async updateGuild(@Param('id') id: string, @Body() body: UpdateGuildDto) {
+        return await this.guildService.updateOne(id, body);
     }
 
 }

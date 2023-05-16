107191904732ace24cee5b075b97adcf8aaf243b
Ajani Green <xjamesdzn@gmail.com>
2023-05-15 21:08:59 -0500

    Initialize payments route

---
+++ src/routes/payments/payments.controller.ts
@@ -0,0 +1,10 @@
+import {Controller} from "@nestjs/common";
+import {PaymentsService} from "./payments.service";
+
+@Controller('payments')
+export class PaymentsController {
+
+    constructor(private readonly paymentsService: PaymentsService) {
+    }
+
+}
---
+++ src/routes/payments/payments.module.ts
@@ -0,0 +1,25 @@
+import {Module} from "@nestjs/common";
+import {MongooseModule} from "@nestjs/mongoose";
+import {PremiumGuildSchema, PremiumUserSchema} from "./payments.schema";
+import {PaymentsController} from "./payments.controller";
+import {PaymentsService} from "./payments.service";
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: 'premium_users',
+                schema: PremiumUserSchema
+            },
+            {
+                name: 'premium_guilds',
+                schema: PremiumGuildSchema
+            }
+        ]),
+    ],
+    controllers: [PaymentsController],
+    providers: [PaymentsService],
+    exports: [PaymentsService]
+})
+export class PaymentsModule {
+}
